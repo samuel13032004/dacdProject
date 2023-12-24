b959f895679da1d64d63346ca75ad0d03687d03f
samueldeniz <sdenizsantana@gmail.com>
2023-12-24 21:19:57 +0000

    BusinessUnit model commit

--- BusinessUnit/src/main/java/org/example/BusinessUnit/control/EventSubscriber.java
+++ BusinessUnit/src/main/java/org/example/BusinessUnit/control/EventSubscriber.java
@@ -6,38 +6,41 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonArray;
 import org.apache.activemq.ActiveMQConnection;
 import org.apache.activemq.ActiveMQConnectionFactory;
+import org.example.BusinessUnit.model.HotelEvent;
+import org.example.BusinessUnit.model.WeatherEvent;
 
 import javax.jms.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class EventSubscriber {
     private static final String WEATHER_TOPIC = "prediction.Weather";
     private static final String HOTEL_TOPIC = "prediction.Hotel";
     private Connection connection;
     private Gson gson;
-
+    private ArrayList<HotelEvent> hotelEvents;
+    private ArrayList<WeatherEvent>weatherEvents;
     public EventSubscriber() {
         this.gson = new Gson();
+        this.hotelEvents = new ArrayList<>();
+        this.weatherEvents = new ArrayList<>();
     }
-
     public void startSubscription() {
         System.out.println("Starting broker subscription...");
-
         try {
             ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(ActiveMQConnection.DEFAULT_BROKER_URL);
             connection = connectionFactory.createConnection();
             connection.start();
             Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-
             // Subscribe to the Weather topic
             Topic weatherTopic = session.createTopic(WEATHER_TOPIC);
             MessageConsumer weatherMessageConsumer = session.createConsumer(weatherTopic);
             weatherMessageConsumer.setMessageListener(message -> processMessage(message, "Weather"));
-
             // Subscribe to the Hotel topic
             Topic hotelTopic = session.createTopic(HOTEL_TOPIC);
             MessageConsumer hotelMessageConsumer = session.createConsumer(hotelTopic);
             hotelMessageConsumer.setMessageListener(message -> processMessage(message, "Hotel"));
-
             System.out.println("Broker subscription initiated.");
         } catch (JMSException e) {
             e.printStackTrace();
@@ -48,22 +51,24 @@ public class EventSubscriber {
             if (message instanceof TextMessage) {
                 String eventJson = ((TextMessage) message).getText();
                 System.out.println("Message received from " + eventType + " topic: " + eventJson);
-
                 try {
                     // Deserialize to JSON object
                     JsonObject jsonObject = gson.fromJson(eventJson, JsonObject.class);
                     System.out.println("Extract data based on the event type");
-
                     // Extract data based on the event type
                     if ("Weather".equals(eventType)) {
                         System.out.println("Extract data based on the event Weather");
-                        extractWeatherData(jsonObject);
+                        WeatherEvent weatherEvent = extractWeatherData(jsonObject);
+                        weatherEvents.add(weatherEvent);
                     } else if ("Hotel".equals(eventType)) {
                         System.out.println("Extract data based on the event Hotel");
-                        extractHotelData(jsonObject);
+                        HotelEvent hotelEvent = extractHotelData(jsonObject);
+                        hotelEvents.add(hotelEvent);
                     } else {
                         System.out.println("Unknown event type: " + eventType);
                     }
+                    System.out.println(weatherEvents.size());
+                    System.out.println(hotelEvents.size());
                 } catch (Exception e) {
                     System.err.println("Error processing JSON: " + e.getMessage());
                     e.printStackTrace();
@@ -74,46 +79,79 @@ public class EventSubscriber {
             e.printStackTrace();
         }
     }
-   private void extractWeatherData(JsonObject jsonObject) {
+   private WeatherEvent extractWeatherData(JsonObject jsonObject) {
        try {
-           // Extract weather data
+           // Extraer datos de la ubicación
+           JsonObject location = jsonObject.getAsJsonObject("location");
+           String island = location.get("island").getAsString();
+           String cityName = location.get("cityName").getAsString();
+
+           // Extraer otros atributos del clima
            double temperature = jsonObject.get("temp").getAsDouble();
-           double humidity = jsonObject.get("humidity").getAsDouble();
+           int humidity = jsonObject.get("humidity").getAsInt();
+           int clouds = jsonObject.get("clouds").getAsInt();
+           double windSpeed = jsonObject.get("windSpeed").getAsDouble();
+           String predictionTime = jsonObject.get("predictionTime").getAsString();
 
-           JsonObject locationObject = jsonObject.getAsJsonObject("location");
-           double latitude = locationObject.get("latitude").getAsDouble();
-           double longitude = locationObject.get("longitude").getAsDouble();
-           String cityName = locationObject.get("cityName").getAsString();
-           String island = locationObject.get("island").getAsString();
-
-           System.out.println("------------------------------------------------------------------");
-           // Do something with the weather data
-           System.out.println("Temperature: " + temperature);
-           System.out.println("Humidity: " + humidity);
-           System.out.println("Latitude: " + latitude);
-           System.out.println("Longitude: " + longitude);
-           System.out.println("City Name: " + cityName);
-           System.out.println("Island: " + island);
+           // Crear instancia de WeatherEvent
+           WeatherEvent weatherEvent = new WeatherEvent(
+                   island,
+                   cityName,
+                   predictionTime,
+                   temperature,
+                   humidity,
+                   clouds,
+                   windSpeed,
+                   new Date() // Debes obtener la fecha del objeto ts si es necesario
+           );
+           return weatherEvent;
        } catch (Exception e) {
            e.printStackTrace();
+           return null;
        }
    }
-
-    private void extractHotelData(JsonObject jsonObject) {
+    private HotelEvent extractHotelData(JsonObject jsonObject) {
         try {
-            // Extract hotel data
-            String hotelName = jsonObject.getAsJsonObject("hotelLocation").get("hotelName").getAsString();
-            JsonArray averagePriceDays = jsonObject.getAsJsonArray("averagePriceDay");
+            // Extraer datos del hotelLocation
+            JsonObject hotelLocation = jsonObject.getAsJsonObject("hotelLocation");
+            String island = hotelLocation.get("island").getAsString();
+            String city = hotelLocation.get("city").getAsString();
+            String hotelName = hotelLocation.get("hotelName").getAsString();
+            String checkout = hotelLocation.get("checkout").getAsString();
 
-            System.out.println("------------------------------------------------------------------");
-            // Do something with the hotel data
-            System.out.println("Hotel Name: " + hotelName);
-            System.out.println("Average Price Days:");
-            for (JsonElement element : averagePriceDays) {
-                System.out.println("  " + element.getAsString());
-            }
+            // Extraer las listas de precios
+            JsonArray averagePriceDays = jsonObject.getAsJsonArray("averagePriceDay");
+            JsonArray cheapPriceDays = jsonObject.getAsJsonArray("cheapPriceDay");
+            JsonArray highPriceDays = jsonObject.getAsJsonArray("highPriceDay");
+
+            // Crear instancia de HotelEvent
+
+            return new HotelEvent(
+                    island,
+                    city,
+                    hotelName,
+                    checkout,
+                    parseStringList(averagePriceDays),
+                    parseStringList(cheapPriceDays),
+                    parseStringList(highPriceDays)
+            );
         } catch (Exception e) {
             e.printStackTrace();
+            return null;
         }
+    }
+    private static List<String> parseStringList(JsonArray jsonArray) {
+        List<String> stringList = new ArrayList<>();
+        for (JsonElement element : jsonArray) {
+            stringList.add(element.getAsString());
+        }
+        return stringList;
+    }
+
+    public ArrayList<HotelEvent> getHotelEvents() {
+        return hotelEvents;
+    }
+    public ArrayList<WeatherEvent> getWeatherEvents() {
+        return weatherEvents;
     }
 }
--- BusinessUnit/src/main/java/org/example/BusinessUnit/model/WeatherEvent.java
+++ BusinessUnit/src/main/java/org/example/BusinessUnit/model/WeatherEvent.java
@@ -3,5 +3,56 @@ package org.example.BusinessUnit.model;
 import java.util.Date;
 
 public class WeatherEvent {
-    public WeatherEvent(String island,String cityName,double temp, int humidity, int clouds, double windSpeed, Date ts,String predictionTime){}
+    private final String island;
+    private final String cityName;
+    private final String predictionTime;
+    private final double temp;
+    private final int humidity;
+    private final int clouds;
+    private final double windSpeed;
+    private final Date ts;
+
+    public WeatherEvent(String island, String cityName, String predictionTime, double temp, int humidity, int clouds, double windSpeed, Date ts){
+
+        this.island = island;
+        this.cityName = cityName;
+        this.predictionTime = predictionTime;
+        this.temp = temp;
+        this.humidity = humidity;
+        this.clouds = clouds;
+        this.windSpeed = windSpeed;
+        this.ts = ts;
+    }
+
+    public String getIsland() {
+        return island;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public String getPredictionTime() {
+        return predictionTime;
+    }
+
+    public double getTemp() {
+        return temp;
+    }
+
+    public int getHumidity() {
+        return humidity;
+    }
+
+    public int getClouds() {
+        return clouds;
+    }
+
+    public double getWindSpeed() {
+        return windSpeed;
+    }
+
+    public Date getTs() {
+        return ts;
+    }
 }
